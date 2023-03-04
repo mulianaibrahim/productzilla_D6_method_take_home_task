01c3fe34938bf35d3d0e9c7b93a47aa82f409569
FIRST_NAME LAST_NAME <mulianaibrahim@gmail.com>
2023-03-04 12:53:00 +0700

    inisiasi project

---
+++ method.js
@@ -0,0 +1,202 @@
+// buatlah minimal 20 PROCEDURE untuk menyimpan nilai untuk 20 variabel 
+let var1 = 4 * 5;
+function setVar1(value) {
+var1 = value;
+}
+
+let var2 = 5 + 5;
+function setVar2(value) {
+var2 = value;
+}
+
+let var3 = 8;
+function setVar3(value) {
+var3 = value;
+}
+
+let var4 = 10 * 2;
+function setVar4(value) {
+var4 = value;
+}
+
+let var5 = 20 -10;
+function setVar5(value) {
+var5 = value;
+}
+
+let var6 = 105 -50 ;
+function setVar6(value) {
+var6 = value;
+}
+
+let var7 = 11 * 14;
+function setVar7(value) {
+var7 = value;
+}
+
+let var8 = 5 ** 2;
+function setVar8(value) {
+var8 = value;
+}
+
+let var9 = 17 ;
+function setVar9(value) {
+var9 = value;
+}
+
+let var10 = 100 / 4;
+function setVar10(value) {
+var10 = value;
+}
+
+let var11 = 19 - 3;
+function setVar11(value) {
+var11 = value;
+}
+
+let var12 = 9 ** 3;
+function setVar12(value) {
+var12 = value;
+}
+
+let var13 = 100 - 49;
+function setVar13(value) {
+var13 = value;
+}
+
+let var14 = 10 / 2;
+function setVar14(value) {
+var14 = value;
+}
+
+let var15 = 10 + 20;
+function setVar15(value) {
+var15 = value;
+}
+
+let var16 = 2 * 3 / 2;
+function setVar16(value) {
+var16 = value;
+}
+
+let var17 = 4 / 2 + 2;
+function setVar17(value) {
+var17 = value;
+}
+
+let var18 = 22 ;
+function setVar18(value) {
+var18 = value;
+}
+
+let var19 = 27 / 3;
+function setVar19(value) {
+var19 = value;
+}
+
+let var20 = 45 * 10;
+function setVar20(value) {
+var20 = value;
+}
+
+
+// Buatlah 20 FUNCTION untuk mengambil nilai dari variable pada soal nomor 1, dan mengembalikan isi dari variablenya
+function getvar1() {
+    return var1
+}
+console.log(getvar1())
+
+function getvar2() {
+    return var2
+}
+console.log(getvar2())
+
+function getvar3() {
+    return var3
+}
+console.log(getvar3())
+
+function getvar4() {
+    return var4
+}
+console.log(getvar4())
+
+function getvar5() {
+    return var5
+}
+console.log(getvar5())
+
+function getvar6() {
+    return var6
+}
+console.log(getvar6())
+
+function getvar7() {
+    return var7
+}
+console.log(getvar7())
+
+function getvar8() {
+    return var8
+}
+console.log(getvar8())
+
+function getvar9() {
+    return var9
+}
+console.log(getvar9())
+
+function getvar10() {
+    return var10
+}
+console.log(getvar10())
+
+function getvar11() {
+    return var11
+}
+console.log(getvar11())
+
+function getvar12() {
+    return var12
+}
+console.log(getvar12())
+
+function getvar13() {
+    return var13
+}
+console.log(getvar13())
+
+function getvar14() {
+    return var14
+}
+console.log(getvar14())
+
+function getvar15() {
+    return var15
+}
+console.log(getvar15())
+
+function getvar16() {
+    return var16
+}
+console.log(getvar16())
+
+function getvar17() {
+    return var17
+}
+console.log(getvar17())
+
+function getvar18() {
+    return var18
+}
+console.log(getvar18())
+
+function getvar19() {
+    return var19
+}
+console.log(getvar19())
+
+function getvar20() {
+    return var20
+}
+console.log(getvar20())
